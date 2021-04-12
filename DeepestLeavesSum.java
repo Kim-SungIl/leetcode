cb93607d843aa06754a3cd8bf2754aa58668741e
Kim-SungIl <joongjum@naver.com>
2021-04-13 00:58:06 +0900

    Update DeepestLeavesSum.java
    add comment

--- DeepestLeavesSum.java
+++ DeepestLeavesSum.java
@@ -18,16 +18,18 @@ class Solution {
         HashMap<Integer, Integer> treeMap = new HashMap<>();
         preOrder(root, 0, treeMap);
         int max = 0;
+	// 가장 큰 depth의 sum을 찾기 위해서 key값중 max값을 구함
         for (int key : treeMap.keySet()) {
-			max = Math.max(max, key);
-		}        
+		max = Math.max(max, key);
+	}        
         return treeMap.get(max);
     }
     
     public void preOrder(TreeNode node, int depth, HashMap<Integer, Integer> map){
         if (node == null){
             return;
-        }        
+        }
+	// tree를 순회하면서 각 depth별로 sum을 구한다.
         if (map.get(depth) == null){
             map.put(depth, node.val);
         }
